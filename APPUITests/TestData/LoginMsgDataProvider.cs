078ab495a697f4d3b4b95fa80aed7a30e710ac62
ajinathfunde <ajinathfunde123@gmail.com>
2025-07-01 14:27:02 +0530

    Added TestData Provider and refinement of code

---
+++ APPUITests/TestData/LoginMsgDataProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APPUITests.TestData
+{
+    public class LoginMsgDataProvider
+    {
+        public static IEnumerable<TestCaseData> SuccessMgs => new List<TestCaseData>
+        {
+            new TestCaseData("Test Login | Practice Test Automation", "Logged In Successfully | Practice Test Automation")              
+        };
+
+        public static IEnumerable<TestCaseData> FailureMgs => new List<TestCaseData>
+        {
+            new TestCaseData("Your password is invalid!")
+        };
+    }
+}
